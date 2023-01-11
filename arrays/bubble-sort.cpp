b57bee43fe26c79a2347b3d46ef2d545105a4988
Dipshan Adhikari <dipshanadhikari1234@gmail.com>
2023-01-11 14:24:03 +0545

    Bubble sort

---
+++ arrays/bubble-sort.cpp
@@ -0,0 +1,43 @@
+// Program to input an array of numbers and return the sorted array using bubble sort
+
+#include <iostream>
+#include <cstdio>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int n;
+
+    cout << "Enter the size of the array:\t";
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("[%d] Enter a number: ", i);
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+        {
+            int temp = arr[j];
+
+            if (arr[j] > arr[j + 1])
+            {
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+
+    cout << "Sorted array:" << endl;
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << endl;
+
+    return 0;
+}
