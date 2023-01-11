c20bc6db972cdeebd27ddd5c407dcff8a78baa21
Dipshan Adhikari <dipshanadhikari1234@gmail.com>
2023-01-11 14:51:06 +0545

    32. Bubble sort

--- arrays/bubble-sort.cpp
+++ arrays/bubble-sort.cpp
@@ -22,8 +22,8 @@ int main()
         cin >> arr[i];
     }
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
         {
             int temp = arr[j];
 
